d5a70e7e7aa1347b0948ef2bf66e4a4f0a4c29a0
chris pucknell <chris.pucknell@gmail.com>
2022-08-08 14:02:50 +0100

    adjust styling, fix issue with profile pic styling

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,34 +3,34 @@ import "./About.css";
 import img from "../../static/img/profile/profile.jpg";
 export const About = () => {
     return (
-        <>
-            <div className="main-container bg-image" id="bg-about">
-                <div className="container">
-                    <div className="card-wrapper">
-                        <div className="card-img">
-                            <img src={img}></img>
-                        </div>
-                        <div className="card-content">
-                            <p>
-                                Something of an introvert, habitually looking off into the distance, I've
-                                never been able to settle on anything for long.
-                                The complex problem solving and creative design
-                                aspects of web development however really get me
-                                hooked. When I'm given a problem to solve it's
-                                fun finding a solution, and I always learn
-                                something along the way. 
-                                <br/><br/>
-                                I have recently completed an intense 13 week
-                                course with Futureproof to turbo charge my
-                                skillset, and now feel confident calling myself
-                                a full-stack developer. I'm looking forward to
-                                new challenges, and the opportunity to grow my
-                                talents further.
-                            </p>
-                        </div>
+        <div className="main-container bg-image" id="bg-about">
+            <div className="container">
+                <div className="card-wrapper">
+                    <div className="card-img">
+                        <img
+                            src={img}
+                            alt="profile pic"
+                        />
+                    </div>
+                    <div className="card-content">
+                        <p className="profile-text">
+                            Something of an introvert, habitually looking off
+                            into the distance, I've never been able to settle on
+                            anything for long. The complex problem solving and
+                            creative design aspects of web development however
+                            really get me hooked. When I'm given a problem to
+                            solve it's fun finding a solution, and I always
+                            learn something along the way.
+                            <br />
+                            <br />I have recently completed an intense 13 week
+                            course with Futureproof to turbo charge my skillset,
+                            and now feel confident calling myself a full-stack
+                            developer. I'm looking forward to new challenges,
+                            and the opportunity to grow my talents further.
+                        </p>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
